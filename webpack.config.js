b024c4e8e035cf132ea0d943241279306b1bba69
Georgi Mavridis <georgimavridis21@gmail.com>
2020-03-15 01:10:10 +0200

    added html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  resolve: {
+    extensions: ["*", ".js"]
   }
 };
